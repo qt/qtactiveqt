a4f7630e481f6734c3e3f568bae9f6b07fb4b8ec
Jonathan Liu <net147@gmail.com>
2011-11-07 20:40:13 +1100

    ActiveQt: Add support for setting properties by reference
    Add support for setting properties by reference to allow properties
    that can only be set by reference to be changed.
    
    Change-Id: I2f39f8720ecd1ba97139ab42756d686db2211cad

---
+++ tests/auto/qaxobject/tst_qaxobject.cpp
@@ -0,0 +1,35 @@
+#include <QtTest/QtTest>
+#include <QAxObject>
+#include <QByteArray>
+
+class tst_QAxObject : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void propertyByRefWritable();
+    void setPropertyByRef();
+};
+
+void tst_QAxObject::propertyByRefWritable()
+{
+    const QAxObject speak("SAPI.SPVoice");
+    const QMetaObject *metaObject = speak.metaObject();
+
+    for (int i = 0; i < metaObject->propertyCount(); ++i) {
+        if (qstrcmp(metaObject->property(i).name(), "Voice")) {
+            QVERIFY(metaObject->property(i).isWritable());
+            break;
+        }
+    }
+}
+
+void tst_QAxObject::setPropertyByRef()
+{
+    QAxObject speak("SAPI.SPVoice");
+
+    QVERIFY(speak.setProperty("Voice", speak.property("Voice")));
+}
+
+QTEST_MAIN(tst_QAxObject)
+#include "tst_qaxobject.moc"
