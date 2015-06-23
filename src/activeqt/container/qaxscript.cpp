qt/qtactiveqt

commit 7dffc4f49a80e1e641a3b04fdd5aee082be39e5f

enable QAxScript on MinGW
[ChangeLog][ActiveQt] Enabled QAxScript on MinGW.

Change-Id: I90b56b02d1c90fada512e725aa6f498861a118f2
Task-number: QTBUG-45022
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@theqtcompany.com>

src/activeqt/container/qaxscript.cpp
@@ -42,7 +42,9 @@
 #include "../shared/qaxutils_p.h"
 
 #if defined(Q_CC_GNU)
-# define QT_NO_QAXSCRIPT
+// Workaround for mingw-w64 bug #464
+// See https://sourceforge.net/p/mingw-w64/bugs/464/
+# define _NO_SCRIPT_GUIDS
 #elif defined(Q_CC_BOR) && __BORLANDC__ < 0x560
 # define QT_NO_QAXSCRIPT
 #endif
@@ -383,8 +385,7 @@ HRESULT WINAPI QAxScriptSite::EnableModeless(BOOL fEnable)
     Direct access to the script engine is provided through
     queryInterface().
 
-    \warning This class is not available with the bcc5.5 and MingW
-    compilers.
+    \warning This class is not available with the bcc5.5 compiler.
 
     \sa QAxScript, QAxScriptManager, QAxBase, {ActiveQt Framework}
 */
@@ -632,8 +633,7 @@ void QAxScriptEngine::addItem(const QString &name)
     error() signal. Direct access to the QAxScriptEngine is provided
     through the scriptEngine() function.
 
-    \warning This class is not available with the bcc5.5 and MingW
-    compilers.
+    \warning This class is not available with the bcc5.5 compiler.
 
     \sa QAxScriptEngine, QAxScriptManager, QAxBase, {ActiveQt Framework}
 */
@@ -901,8 +901,7 @@ QAxBase *QAxScript::findObject(const QString &name)
     using addObject(). Then load() the script sources and invoke the
     functions using call().
 
-    \warning This class is not available with the bcc5.5 and MingW
-    compilers.
+    \warning This class is not available with the bcc5.5 compiler.
 
     \sa QAxScript, QAxScriptEngine, QAxBase, {ActiveQt Framework}
 */

commit 66e1ecc28e8bcb0b92ea0212caf611d99a80b0ce

fix build with MinGW header version > 3.0
Commit 7dffc4f4 broke the compilation with MinGW64 3.1 and above.
The work-around for mingw-w64 bug #464 must only applied to
MinGW versions that are affected.

Task-number: QTBUG-46712
Change-Id: I62bda4db361d15849fa071d0acaee72254d34bd6
Reviewed-by: Oliver Wolff <oliver.wolff@theqtcompany.com>

src/activeqt/container/qaxscript.cpp
@@ -41,7 +41,7 @@
 #include "qaxscript.h"
 #include "../shared/qaxutils_p.h"
 
-#if defined(Q_CC_GNU)
+#if defined(Q_CC_GNU) && __MINGW64_VERSION_MAJOR == 3 && __MINGW64_VERSION_MINOR > 0
 // Workaround for mingw-w64 bug #464
 // See https://sourceforge.net/p/mingw-w64/bugs/464/
 # define _NO_SCRIPT_GUIDS