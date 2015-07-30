qt/qtactiveqt

commit 66e1ecc28e8bcb0b92ea0212caf611d99a80b0ce

fix build with MinGW header version > 3.0
Commit 7dffc4f4 broke the compilation with MinGW64 3.1 and above.
The work-around for mingw-w64 bug #464 must only applied to
MinGW versions that are affected.

Task-number: QTBUG-46712
Change-Id: I62bda4db361d15849fa071d0acaee72254d34bd6
Reviewed-by: Oliver Wolff <oliver.wolff@theqtcompany.com>

src/activeqt/container/qaxscript.cpp
@@ -41,7 +41,7 @@
 #include "qaxscript.h"
 #include "../shared/qaxutils_p.h"
 
-#if defined(Q_CC_GNU)
+#if defined(Q_CC_GNU) && __MINGW64_VERSION_MAJOR == 3 && __MINGW64_VERSION_MINOR > 0
 // Workaround for mingw-w64 bug #464
 // See https://sourceforge.net/p/mingw-w64/bugs/464/
 # define _NO_SCRIPT_GUIDS

commit 46159b840cfcfeaea5b1c78d1d0e4fa40d679366

fix build with MinGW header version >= 4.0
Commit 66e1ecc2 broke the compilation with MinGW64 4.0 and above.
The work-around for mingw-w64 bug #464 must be applied to
all MinGW versions that are affected.

Change-Id: Ie6665d076dc1e38e459ae3c9cc51202b4a12e736
Reviewed-by: Oliver Wolff <oliver.wolff@theqtcompany.com>

src/activeqt/container/qaxscript.cpp
@@ -41,7 +41,7 @@
 #include "qaxscript.h"
 #include "../shared/qaxutils_p.h"
 
-#if defined(Q_CC_GNU) && __MINGW64_VERSION_MAJOR == 3 && __MINGW64_VERSION_MINOR > 0
+#if defined(Q_CC_GNU) && (__MINGW64_VERSION_MAJOR == 3 && __MINGW64_VERSION_MINOR > 0 || __MINGW64_VERSION_MAJOR >= 4)
 // Workaround for mingw-w64 bug #464
 // See https://sourceforge.net/p/mingw-w64/bugs/464/
 # define _NO_SCRIPT_GUIDS