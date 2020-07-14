88b4ca5ea5b7a20cb4d44d45eb4cb0b777bb9201
Volker Hilsheimer <volker.hilsheimer@qt.io>
2020-07-14 14:55:13 +0200

    Print warning message when we can't get type information
    Task-number: QTBUG-85467
    Pick-to: 5.15
    Change-Id: I47920180626ea3ecbda3ea8e59600bf2f653f7fe
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    Reviewed-by: Andy Shaw <andy.shaw@qt.io>

--- src/activeqt/container/qaxbase.cpp
+++ src/activeqt/container/qaxbase.cpp
@@ -2092,7 +2092,7 @@ void MetaObjectGenerator::readClassInfo()
     if (dispInfo && !typelib)
         dispInfo->GetContainingTypeLib(&typelib, &index);
 
-    if (!typelib) {
+    if (!typelib && !that->control().isEmpty()) {
         QSettings controls(QLatin1String("HKEY_LOCAL_MACHINE\\Software"), QSettings::NativeFormat);
         QString tlid = controls.value(QLatin1String("/Classes/CLSID/") + that->control() + QLatin1String("/TypeLib/.")).toString();
         QString tlfile;
@@ -2159,8 +2159,11 @@ void MetaObjectGenerator::readClassInfo()
         }
     }
 
-    if (!d || !dispInfo || !cacheKey.isEmpty() || !d->tryCache)
+    if (!d || !dispInfo || !cacheKey.isEmpty() || !d->tryCache) {
+        if (disp && !dispInfo)
+            qWarning("%s: IDispatch %p does not provide interface information", Q_FUNC_INFO, disp);
         return;
+    }
 
     TYPEATTR *typeattr = nullptr;
     dispInfo->GetTypeAttr(&typeattr);
