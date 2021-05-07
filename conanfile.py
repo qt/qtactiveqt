c32e976ffccd6731b4a81068fc2e90ba0ddd57fd
Jani Heikkinen <jani.heikkinen@qt.io>
2021-05-07 10:46:26 +0300

    Bump version
    Change-Id: If3a8f0033cf7b3408c842db38c44cf9876d02003

--- conanfile.py
+++ conanfile.py
@@ -37,7 +37,7 @@ class QtConanError(Exception):
 
 class QtActiveQtFormats(ConanFile):
     name = "qtactiveqt"
-    version = "6.1.0"
+    version = "6.1.1"
     license = "LGPL-3.0+, GPL-2.0+, Commercial Qt License Agreement"
     author = "The Qt Company <https://www.qt.io/contact-us>"
     url = "https://code.qt.io/cgit/qt/qtactiveqt.git"
