2ca4077753698a9ff14b84165b7195d6d0a4a52a
Marc Mutz <marc.mutz@qt.io>
2022-12-20 12:27:49 +0100

    Port from <cctype> toupper() to QtMiscUtils::toAsciiUpper()
    The C toupper function is locale-dependent. Given the right locale
    (Türkiye, e.g.), toupper(i) is either
    
    - İ (LATIN CAPITAL LETTER I WITH DOT ABOVE; if representable) or
    - i (unchanged; if it isn't)
    
    Both results are wrong for the use-cases at hand.
    
    Task-number: QTBUG-109235
    Change-Id: Ia9726f0079c5f2625d97a341836b3d4505db6be1
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>
    Reviewed-by: Samuel Ghinet <samuel.ghinet@qt.io>
    (cherry picked from commit 09b4615abcdbef7087bb9862a3e73f3bb7930768)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/activeqt/container/qaxbase.cpp
+++ src/activeqt/container/qaxbase.cpp
@@ -78,6 +78,7 @@
 #include <private/qobject_p.h>
 #include <private/qmetaobject_p.h>
 #include <private/qmetaobjectbuilder_p.h>
+#include <private/qtools_p.h>
 
 #include <qt_windows.h>
 #include <ocidl.h>
@@ -2288,7 +2289,7 @@ void MetaObjectGenerator::addSetterSlot(const QByteArray &property)
     if (isupper(prototype.at(0))) {
         prototype.insert(0, "Set");
     } else {
-        prototype[0] = char(toupper(prototype[0]));
+        prototype[0] = QtMiscUtils::toAsciiUpper(prototype[0]);
         prototype.insert(0, "set");
     }
     const QByteArray type = propertyType(property);
@@ -2486,7 +2487,7 @@ void MetaObjectGenerator::readFuncsInfo(ITypeInfo *typeinfo, ushort nFuncs)
                     set = "Set";
                 } else {
                     set = "set";
-                    prototype[0] = char(toupper(prototype[0]));
+                    prototype[0] = QtMiscUtils::toAsciiUpper(prototype[0]);
                 }
 
                 prototype = set + prototype;
