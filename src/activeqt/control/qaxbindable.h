1e198c3d2bbec4aa6c1777875d29168d7a054bab
Tarja Sundqvist <tarja.sundqvist@qt.io>
2023-10-19 21:35:46 +0300

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the files in tqtc-qtactiveqt. Examples, tests, or
    documentation files were not updated.
    
    Task-number: QTQAINFRA-5900
    Change-Id: I0a441106d1522003177274555a108b66d1e22c27
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/activeqt/control/qaxbindable.h
+++ src/activeqt/control/qaxbindable.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2015 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #ifndef QAXBINDABLE_H
 #define QAXBINDABLE_H
