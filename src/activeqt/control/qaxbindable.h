41d072cd6de9ff6a15804f5f67a3fdfb8bcb5baf
Tarja Sundqvist <tarja.sundqvist@qt.io>
2024-01-11 21:35:25 +0200

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit 1e198c3d2bbec4aa6c1777875d29168d7a054bab.
    
    Revert of commercial SPDX license identifiers is required for the
    Qt 6.5.x opensource releases, Qt 6.5.4 onwards.
    
    Change-Id: I5e0225ee7784cf38713dd20dd3b28b9c10c2deb8
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

--- src/activeqt/control/qaxbindable.h
+++ src/activeqt/control/qaxbindable.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2015 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 #ifndef QAXBINDABLE_H
 #define QAXBINDABLE_H
--- src/activeqt/shared/qaxtypes_p.h
+++ src/activeqt/shared/qaxtypes_p.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2015 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
 #ifndef QAXTYPES_H
 #define QAXTYPES_H
