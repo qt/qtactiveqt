93655dd5d18da5483799f26f28262ae452b8f5af
Oliver Wolff <oliver.wolff@qt.io>
2025-12-08 09:11:29 +0100

    dumpcpp: Reflect update of moc's OutputRevision
    The latest additions to moc's output (class definition hash) are
    handled automatically as we call moc.exe directly.
    
    Fixes: QTBUG-142519
    Pick-to: 6.11
    Change-Id: I35a0e8d49a2766139b67f07c1675a5ad7ee13e79
    Reviewed-by: Olivier De Cannière <olivier.decanniere@qt.io>
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- tools/dumpcpp/main.cpp
+++ tools/dumpcpp/main.cpp
@@ -535,7 +535,7 @@ bool generateClassImpl(QTextStream &out, const QMetaObject *mo, const QByteArray
                        bool useControlName,
                        QString *errorString)
 {
-    Q_STATIC_ASSERT_X(QMetaObjectPrivate::OutputRevision == 13, "dumpcpp should generate the same version as moc");
+    Q_STATIC_ASSERT_X(QMetaObjectPrivate::OutputRevision == 14, "dumpcpp should generate the same version as moc");
 
     QByteArray qualifiedClassName;
     if (!nameSpace.isEmpty())
